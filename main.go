14f168d24446a44171feb877454d3b3ac2ac05fb
jungmini <jungmini0601@gmail.com>
2024-06-05 13:37:32 +0900

    URL Path base routing 추가

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"strings"
+
 	// Uncomment this block to pass the first stage
 	"net"
 	"os"
@@ -14,16 +16,39 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-
-	fmt.Println("Server Started to Port: 4221")
-	fmt.Println("Wait for Connection...")
+	// 커넥션 받을 때 까지 대기
 	conn, err := l.Accept()
-	fmt.Println("Connected!")
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	// HTTP Parse
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error acceptiong connection: ", err)
+	}
+
+	req := string(buf)
+	path := parsePath(req)
+	res := makeResponseFromPath(path)
+	conn.Write([]byte(res))
 	fmt.Println("Server Stop")
+}
+
+func parsePath(req string) string {
+	lines := strings.Split(req, "\r\n")
+	path := strings.Split(lines[0], " ")[1]
+	return path
+}
+
+func makeResponseFromPath(path string) string {
+	var res string
+	if path == "/" {
+		res = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		res = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	return res
 }
